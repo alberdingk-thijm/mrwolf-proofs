7a45a6f1a0f967fe931fbb718da277d9577d09b6
Tim Alberdingk Thijm <tim.thijm@gmail.com>
2023-04-22 00:24:08 -0400

    Work on timepiece soundness more

--- Timepiece.v
+++ Timepiece.v
@@ -4,11 +4,38 @@ Require Import Coq.Lists.List.
 Require Import Coq.Logic.FunctionalExtensionality.
 Require Import Lia.
 
+Lemma map_combine :
+  forall {T1 T2 T3 T4 : Type} (l1 : list T1) (l2 : list T2) (f1 : T1 -> T3) (f2 : T2 -> T4),
+    map (fun p => (f1 (fst p), f2 (snd p))) (combine l1 l2) =
+      combine (map f1 l1) (map f2 l2).
+Proof.
+  intros; generalize dependent l2.
+  induction l1; auto.
+  intros l2.
+  rewrite map_cons.
+  induction l2; auto.
+  simpl.
+  rewrite IHl1. reflexivity.
+Qed.
+
+Lemma self_combine :
+  forall { A B C : Type } (l : list A) (f1 : A -> B) (f2 : A -> C),
+    map (fun (x : A * A) => (f1 (fst x), f2 (snd x))) (combine l l)
+        = map (fun (x : A) => (f1 x, f2 x)) l.
+Proof.
+  induction l.
+  - auto.
+  - intros f1 f2.
+    simpl.
+    rewrite (IHl f1 f2).
+    auto.
+Qed.
+
 Section timepiece.
   Variable S : Type.
   Definition Node := nat.
   Definition Edge : Type := (Node * Node).
-  Definition Graph : Type := list (list Node).
+  Definition Graph : Type := (list Node * list Edge).
 
   Variable I : Node -> S.
   Variable Merge : S -> S -> S.
@@ -21,6 +48,28 @@ Section timepiece.
 
   Variable A : Node -> Q.
 
+  (** Return all nodes that have an outgoing edge to the given node. *)
+  Definition predecessors (g : Graph) (node : Node) : list Node :=
+    map (fun e => fst e) (filter (fun e => snd e =? node) (snd g)).
+
+  Lemma in_predecessors :
+    forall (g : Graph) (u v : Node),
+      In u (fst g) /\ In (u, v) (snd g) -> In u (predecessors g v).
+  Proof.
+    intros g u v [Hunode Huvedge].
+    unfold predecessors.
+    replace u with (fst (u, v)) by reflexivity.
+    apply (in_map fst (filter (fun e : Node * nat => snd e =? v) (snd g))).
+    apply filter_In.
+    split.
+    + apply Huvedge.
+    + apply PeanoNat.Nat.eqb_refl.
+  Qed.
+
+  (* all edges in a graph belong to real nodes *)
+  Definition well_formed_graph (g : Graph) :=
+    forall (n m : Node), In (n, m) (snd g) -> In n (fst g) /\ In m (fst g).
+
   (* Computing a new route at a node given routes of its neighbors. *)
   Definition updated_state (node : Node)
     (neighbors : list (Node * S)) : S :=
@@ -31,7 +80,7 @@ Section timepiece.
   Fixpoint σ (g : Graph) (node : Node) (t : nat) : S :=
     match t with
       | O => I node
-      | Datatypes.S t' => updated_state node (map (fun m => (m, σ g m t')) (nth node g nil))
+      | Datatypes.S t' => updated_state node (map (fun m => (m, σ g m t')) (predecessors g node))
     end.
 
   Definition initial_condition (node : Node) := A node 0 (I node).
@@ -49,29 +98,62 @@ Section timepiece.
 
   (* The original inductive condition for a node [n]. *)
   Definition inductive_condition
-    (n : Node) (neighbors : list Node) :=
+    (n : Node) (g : Graph) :=
     forall (t : nat) (states : list S),
-      length states = length neighbors ->
+      length states = length (predecessors g n) ->
       inductive_condition_untimed
         n (A n (1 + t))
-        (combine neighbors states) (map (fun m => (A m t)) neighbors).
+        (combine (predecessors g n) states) (map (fun m => (A m t)) (predecessors g n)).
 
   Definition inductive_invariant
-    (n : Node) (neighbors : list Node) :=
-    initial_condition n /\ inductive_condition n neighbors.
+    (n : Node) (g : Graph) :=
+    initial_condition n /\ inductive_condition n g.
 
   Lemma inductive_condition_is_sound :
     forall (g : Graph) (v : Node),
-        v < length g ->
-        Forall (fun u => u < length g) (nth v g nil) ->
-        inductive_invariant v (nth v g nil) ->
-        forall (t : nat), A v t (σ g v t).
+      well_formed_graph g ->
+      In v (fst g) ->
+      inductive_invariant v g ->
+      forall (t : nat), A v t (σ g v t).
   Proof.
-    intros g v Hving Huing Hindinv t.
+    intros g v Hg Hving Hindinv t.
     generalize dependent v.
-    induction t; intros v Hving Huing [Hinitial Hinductive].
+    induction t; intros v Hving [Hinitial Hinductive].
     - assumption. (* by Hinitial *)
-    - simpl.
+    - simpl in *.
+      unfold inductive_condition, inductive_condition_untimed in Hinductive.
+      specialize (Hinductive t (map (fun m => σ g m t) (predecessors g v))).
+      rewrite combine_length in Hinductive.
+      rewrite map_length in Hinductive.
+      rewrite map_length in Hinductive.
+      rewrite PeanoNat.Nat.min_id in Hinductive.
+      replace (combine (predecessors g v)
+                       (map (fun m : Node => σ g m t) (predecessors g v)))
+                with (map (fun m : Node => (id m, σ g m t)) (predecessors g v)) in Hinductive.
+      apply Hinductive; auto.
+      2: {
+        rewrite <- (map_id (predecessors g v)).
+        rewrite map_map.
+        rewrite map_map.
+        rewrite <- map_combine.
+        rewrite <- self_combine.
+        reflexivity.
+      }
+      rewrite <- map_combine.
+      replace (map (fun p : Node * Node => (id (fst p), σ g (fst p) t, A (snd p) t))
+       (combine (predecessors g v) (predecessors g v))) with
+        (map (fun u : Node => (u, σ g u t, A u t)) (predecessors g v)).
+      2: {
+        induction (predecessors g v).
+        - auto.
+        - simpl.
+      }
+      eapply Forall_impl.
+      rewrite Forall_map.
+      apply Forall_forall.
+      intros x Hxin.
+      apply in_combine_l in Hxin.
+      simpl in Hinductive.
       induction (nth v g nil).
       + simpl. unfold inductive_condition in Hinductive.
         simpl in Hinductive. unfold inductive_condition_untimed in Hinductive.
